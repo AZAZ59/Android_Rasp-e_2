2056f0f399df71863bdff4f0e6c9dc8496933387
YOUR NAME <Paradox11>
2015-05-27 21:09:44 +0400

    add styles

--- app/src/main/java/com/example/azaz/myapplication/schedule.java
+++ app/src/main/java/com/example/azaz/myapplication/schedule.java
@@ -44,13 +44,14 @@ public class schedule extends ActionBarActivity implements Handable {
 
         for (int i = 1; i < 13; i++) {
             int current=0;
+            View page = inflater.inflate(R.layout.activity_list__schedule, null);
             String[] arr = new String[45];
             for(int j=0;j<schedule.size();j++) {
                 if(schedule.get(j).getDay()==i){
                     Schedule_item item = schedule.get(j);
                     arr[current]+=item.getName()+"\n"+item.getCab()+"\n"+item.getTeacher();
                 }
-                View page = inflater.inflate(R.layout.activity_list__schedule, null);
+
                 TextView textView = (TextView) page.findViewById(R.id.TEST_TEXT);
                 if(i<=6){
                     textView.setText("Неделя 1, день "+i);
@@ -58,14 +59,14 @@ public class schedule extends ActionBarActivity implements Handable {
                     textView.setText("Неделя 2, день "+i%6);
                 }
 
-                GridView gw = (GridView) page.findViewById(R.id.gridView);
             }
-            
+            GridView gw = (GridView) page.findViewById(R.id.gridView);
             //DataAdapter da = new DataAdapter(arr,getApplicationContext(),R.id.TEST_TEXT);
             ArrayAdapter<String> st = new ArrayAdapter<String>(this, R.layout.activity_schedule__item, R.id.schedule_Item_text, arr);
             gw.setAdapter(st);
             pages.add(page);
         }
+
 
         ViewPager viewPager = new ViewPager(this);
         viewPager.setAdapter(new SimplePageAdapter(pages));
